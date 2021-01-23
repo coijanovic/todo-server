3e53906cbfc63e9e9435c4b4ad9a87ebcf3d4f7d
Christoph Coijanovic <mail@coijanovic.com>
2021-01-23 15:51:12 +0100

    changed todos to json

--- todo-server.py
+++ todo-server.py
@@ -1,42 +1,72 @@
-from flask import Flask, Response, request, redirect, url_for
+from flask import Flask, Response, request, redirect, url_for, jsonify
 import os.path
-import yaml
+import json
+import time
 import fileinput
 
-if os.path.exists('config.yaml'):
-    configfile = 'config.yaml'
+if os.path.exists('config.json'):
+    configfile = 'config.json'
 else:
-    configfile = 'defaultconfig.yaml'
+    configfile = 'defaultconfig.json'
 
 with open(configfile, 'r') as f:
-    try:
-        config = yaml.safe_load(f)
-    except yaml.YAMLError as exc:
-        print(exc)
+    config = json.load(f)
 
 app = Flask(__name__)
 app.static_folder = 'static'
+
+def get_todos():
+    with open(config['todofile'], 'r') as f:
+        todos = json.load(f)
+    return todos
+
+def write_todos(data):
+    with open(config['todofile'], "w") as f:
+        json.dump(data, f)
+
+def format_todos(todos):
+    ret = ""
+    for i in todos:
+        ret += "[{p}] - {c}\n".format(p=i['prio'],c=i['content'])
+    return ret
 
 @app.route('/')
 def index():
     msg = 'web version wip'
     return Response(msg, mimetype='text/plain')
 
-@app.route('/tasks')
-def get_tasks():
-    todofile = open(config['todopath'], 'r')
-    todos = [ line for line in todofile if '[ ]' in line ]
-    todos.sort(reverse=True)
-    return Response(todos, mimetype='text/plain')
+@app.route('/todos/open')
+def get_open_todos():
+    todos = get_todos()
+    open_todos = [ t for t in todos if t['done'] == False ]
+    open_todos.sort(key=lambda k: k['prio'], reverse=True)
+    return format_todos(open_todos)
 
 @app.route('/add', methods = ['GET', 'POST'])
 def add_task():
-    todo = request.json
-    
-    todofile = open(config['todopath'], 'a')
-    todofile.write("[{prio}] - {content} - [ ]\n".format(prio=todo['prio'], content=todo['content']))
-    todofile.close()
-    return Response('done', mimetype='text/plain')
+    r = request.json
+    todo = {
+        "prio": r['prio'],
+        "content": r['content'],
+        "done": False,
+        "time": {
+            "created": int(time.time()),
+            "edited": int(time.time())
+        }
+    }
+    todos = get_todos()
+    todos.append(todo)
+    write_todos(todos)
+    return Response('done\n', mimetype='text/plain')
+
+@app.route('/done/<content>')
+def mark_done(content):
+    todos = get_todos()
+    for t in todos:
+        if t['content'] == content:
+            t['done'] = True
+    write_todos(todos)
+    return Response('done\n', mimetype='text/plain')
 
 if __name__ == '__main__':
     app.run(debug=False, host=config['hostip'], port=config['hostport'])
